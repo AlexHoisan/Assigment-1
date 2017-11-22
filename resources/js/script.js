5f644e24f2b6ca29478bfd610289b5888e6afb27
Hoisan Alex <hoisan_alex@yahoo.com>
2017-11-22 17:09:45 +0100

    added cart

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,6 +17,8 @@ $('.js--nav-icon').click(function(){
 
 /* Shopping Cart */
 
+
+
 $(".add-to-cart").click(function(event){
 	event.preventDefault();
 	var name = $(this).attr("data-name");
@@ -26,37 +28,146 @@ $(".add-to-cart").click(function(event){
 	
 	
 	
-	addItemToCart(name, price, count);
+	shoppingCart.addItemToCart(name, price, count);
 	displayCart();
 });
 
 $("#clear-cart").click(function(event){
-	clearCart();
+	shoppingCart.clearCart();
 	displayCart();
 });
 
 function displayCart() {
-	var cartArray = listCart();
+	var cartArray = shoppingCart.listCart();
 	var output = "";
+	var position = 1;
 	for (var i in cartArray) {
-		output += "<li>"+cartArray[i].name+" "+"<span>"+cartArray[i].count+"</span>"+"</li>";
+		output += "<tr>"+"<td>"+"<button class='delete-item' data-name='"+cartArray[i].name+"'>X</button>"+"<p>"+position+"</p>"+"</td>"+"<td>"+cartArray[i].name+"</td>"+"<td>"+"<span>"+cartArray[i].count+"</span>"+"<div class='add-remove-buttons'>"+" <button class='plus-item' data-name='"+cartArray[i].name+"'> + </button> "+"<button class='substract-item' data-name='"+cartArray[i].name+"'> - </button> "+"</div>"+"</td>"+"<td>"+cartArray[i].price+"</td>"+"<td>"+cartArray[i].total+"</td>"+"</tr>";
+		
+		position ++;
 	}
 	$("#show-cart").html(output);
-	$("#total-cart").html(totalCart());
-};
+	$("#count-cart").html(shoppingCart.countCart())
+	$("#total-cart").html(shoppingCart.totalCart());
+}
+
+$("#show-cart").on("click", ".delete-item", function(event) {
+	var name = $(this).attr("data-name");
+	shoppingCart.removeItemFromCartAll(name);
+	displayCart();
+});
+
+$("#show-cart").on("click", ".plus-item", function(event) {
+	var name = $(this).attr("data-name");
+	shoppingCart.addItemToCart(name, 0, 1);
+	displayCart();
+});
+
+$("#show-cart").on("click", ".substract-item", function(event) {
+	var name = $(this).attr("data-name");
+	shoppingCart.removeItemFromCart(name);
+	displayCart();
+});
+
+
 
 /***************************************/
 // Shopping cart functions
 /* Scope - Determins where a variable or a value is visible */ 
 
-var cart = [];
-//when you use the sintax below, it actually generates an object
+var shoppingCart = {};
+shoppingCart.cart = [];
 
-var Item = function(name, price, count) {
+shoppingCart.Item = function(name, price, count) {
 	this.name = name
 	this.price = price
 	this.count = count
 };
+
+shoppingCart.addItemToCart = function(name, price, count) {
+	for (var i in this.cart) {
+		if (this.cart[i].name === name) {
+			this.cart[i].count += count;
+			this.saveCart();
+			return;
+		}
+	}
+	var item = new this.Item(name, price, count);
+	this.cart.push(item);
+	this.saveCart();
+};
+
+shoppingCart.removeItemFromCart = function(name) {
+	for (var i in  this.cart) {
+		if (this.cart[i].name === name) {
+			this.cart[i].count --;
+			if (this.cart[i].count === 0) {
+				this.cart.splice(i, 1); //it removes "1" item from the array
+			}
+			break;
+		}
+	}
+	this.saveCart();
+};
+
+shoppingCart.removeItemFromCartAll = function(name) {
+	for (var i in this.cart) {
+		if (this.cart[i].name === name) {
+			this.cart.splice(i, 1);
+			break;
+		}
+	}
+	this.saveCart();
+};
+
+shoppingCart.clearCart = function() {
+	this.cart = [];
+	this.saveCart();
+};
+
+shoppingCart.countCart = function() {
+	var totalCount = 0;
+	for (var i in this.cart){
+		totalCount += this.cart[i].count;
+	}
+	return totalCount;
+};
+
+shoppingCart.totalCart = function() {
+	var totalCost = 0;
+	for (var i in this.cart) {
+		totalCost += this.cart[i].price * this.cart[i].count;
+	}
+	return totalCost.toFixed(2);
+};
+
+shoppingCart.listCart = function() {
+	/*return cart.slice(); -> here, the slice property makes a copy of the array; but still reference the objects inside*/
+	var cartCopy = [];
+	for (var i in this.cart) {
+		var item = this.cart[i];
+		var itemCopy = {};
+		for (var p in item) {
+			itemCopy[p] = item[p];
+		}
+		itemCopy.total = (item.price * item.count).toFixed(2);
+		cartCopy.push(itemCopy);
+	}
+	return cartCopy;
+};
+
+shoppingCart.saveCart = function() {
+	localStorage.setItem("shoppingCart", JSON.stringify(this.cart));
+};
+
+shoppingCart.loadCart = function() {
+	this.cart = JSON.parse(localStorage.getItem("shoppingCart"));
+};
+
+shoppingCart.loadCart();
+displayCart();
+
+//when you use the sintax below, it actually generates an object
 
 /*
 // new item for shopping cart
@@ -67,104 +178,23 @@ cart.push(apple);*/
 
 // addItemToCart(name, price, count)
 
-function addItemToCart(name, price, count) {
-	for (var i in cart) {
-		if (cart[i].name === name) {
-			cart[i].count += count;
-			saveCart();
-			return;
-		}
-	}
-	var item = new Item(name, price, count);
-	cart.push(item);
-	saveCart();
-};
-
 // removeItemFromCart(name) // Removes one item
-
-function removeItemFromCart(name) {
-	for (var i in  cart) {
-		if (cart[i].name === name) {
-			cart[i].count --;
-			if (cart[i].count === 0) {
-				cart.splice(i, 1); //it removes "1" item from the array
-			}
-			return;
-		}
-	}
-	saveCart();
-}
 
 // removeItemFromCartAll(name) //Removes all items with name
 
-function removeItemFromCartAll(name) {
-	for (var i in cart) {
-		if (cart[i].name === name) {
-			cart.splice(i, 1);
-			break;
-		}
-	}
-	saveCart();
-}
-
-
 //clearCart()
-
-function clearCart() {
-	cart = [];
-	saveCart();
-}
 
 //countCart() -> Returns total count
 
-function countCart() {
-	var totalCount = 0;
-	for (var i in cart){
-		totalCount += cart[i].count;
-	}
-	return totalCount;
-}
-
 //totalCart() -> Returns the total cost
-
-function totalCart() {
-	var totalCost = 0;
-	for (var i in cart) {
-		totalCost += cart[i].price * cart[i].count;
-	}
-	return totalCost;
-}
 
 //listCart() -> an array of items
 
-function listCart() {
-	/*return cart.slice(); -> here, the slice property makes a copy of the array; but still reference the objects inside*/
-	var cartCopy = [];
-	for (var i in cart) {
-		var item = cart[i];
-		var itemCopy = {};
-		for (var p in item) {
-			itemCopy[p] = item[p];
-		}
-		cartCopy.push(itemCopy);
-	}
-	return cartCopy;
-}
-
 //saveCart() -> Local
-
-function saveCart() {
-	localStorage.setItem("shoppingCart", JSON.stringify(cart));
-}
 
 //loadCart() -> loads the cart when page is opened
 
-function loadCart() {
-	cart = JSON.parse(localStorage.getItem("shoppingCart"));
-}
 
-loadCart();
-displayCart();
 
 
 
